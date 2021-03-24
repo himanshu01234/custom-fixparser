f72da5dbf4731e95313003c22928fc127e92d773
himanshu01234 <himanshu.goel12345@gmail.com>
2021-03-24 16:49:15 +0530

    custom fixparser

--- src/handler/FIXParserClientBase.ts
+++ src/handler/FIXParserClientBase.ts
@@ -42,10 +42,9 @@ export default class FIXParserClientBase extends EventEmitter {
     public startHeartbeat() {
         this.stopHeartbeat();
         this.heartBeatIntervalId = setInterval(() => {
-            console.log("Heart beat function", this.fixVersion);
             const heartBeat = this.fixParser!.createMessage(
                 new Field(8, this.fixVersion),
-                new Field(Fields.MsgType, 0),
+                new Field(Fields.MsgType, 'A'),
                 new Field(
                     Fields.MsgSeqNum,
                     this.fixParser!.getNextTargetMsgSeqNum(),
@@ -53,6 +52,9 @@ export default class FIXParserClientBase extends EventEmitter {
                 new Field(Fields.SenderCompID, this.sender),
                 new Field(Fields.SendingTime, this.fixParser!.getTimestamp()),
                 new Field(Fields.TargetCompID, this.target),
+                new Field(Fields.Username, 'VertoAPI'),
+                new Field(Fields.Password, 'qzG6jXL6BU'),
+                new Field(Fields.EncryptMethod, '0'),
             );
             this.send(heartBeat);
         }, this.heartBeatInterval!);
